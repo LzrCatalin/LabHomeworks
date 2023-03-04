7e60a82b67a6b81a57e023d3c4d78b298c58ca76
Razvan Lazar <grigore.razvan.lazar@gmail.com>
2023-03-04 08:59:39 +0200

    Fix problem1 implementation.

--- problem1.c
+++ problem1.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 
+/**
+ * Giving 3 numbers, print the number with the most even digits.
+ */
+
 int main(){
 
     int number_1, number_2, number_3, count_1 = 0, count_2 = 0, count_3 = 0;
+	int copy_number_1, copy_number_2, copy_number_3;
 
-    // Enter value for each number from keyboard
-    
     // Number 1 value from keyboard
     printf("Enter value of number_1:");
     scanf("%d", &number_1);
@@ -18,10 +21,15 @@ int main(){
     printf("Enter value of number_3:");
     scanf("%d", &number_3);
 
+	// Save numbers for final print
+	copy_number_1 = number_1;
+	copy_number_2 = number_2;
+	copy_number_3 = number_3;
+
     // Count how many even digits each number has
 
     // Number 1
-    while (number_1 != 0){
+    while (number_1 != 0) {
         if ((number_1 % 10) % 2 == 0){
             count_1++;
         }
@@ -29,7 +37,7 @@ int main(){
     }
 
     // Number 2
-    while (number_2 != 0){
+    while (number_2 != 0) {
         if ((number_2 % 10) % 2 == 0){
             count_2++;
         }
@@ -37,7 +45,7 @@ int main(){
     }
 
     // Number 3
-    while (number_3 != 0){
+    while (number_3 != 0) {
         if ((number_3 % 10) % 2 == 0){
             count_3++;
         }
@@ -47,21 +55,21 @@ int main(){
     // Printing how many even digits each number has
 
     // Number 1
-    printf("\nCount even digits number_1: %d", count_1);
+    printf("Count even digits number_1: %d\n", count_1);
 
     // Number 2
-    printf("\nCount even digits number_2: %d", count_2);
+    printf("Count even digits number_2: %d\n", count_2);
 
     // Number 3
-    printf("\nCount even digits number_3: %d", count_3);
+    printf("Count even digits number_3: %d\n", count_3);
 
     // Check and print the number with the most even digits
     if (count_1 > count_2 && count_1 > count_3){
-        printf("Result: %d", number_1);
+        printf("Result: %d\n", copy_number_1);
     }else if (count_2 > count_3){
-        printf("Result: %d", number_2);
+        printf("Result: %d\n", copy_number_2);
     }else{
-        printf("Result: %d", number_3);
+        printf("Result: %d\n", copy_number_3);
     }
 
     return 0;  
